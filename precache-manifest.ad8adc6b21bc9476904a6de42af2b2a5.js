a56a74f99b6abc39ef8829db398d62543982d66e
ldail <leondailofficial@gmail.com>
2020-04-07 16:46:16 -0700

    progress

---
+++ precache-manifest.ad8adc6b21bc9476904a6de42af2b2a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146204d379d6eeed27bfaa4e16185ac6",
+    "url": "/generations-client/index.html"
+  },
+  {
+    "revision": "054416f6e87ad85ff913",
+    "url": "/generations-client/static/css/main.71471504.chunk.css"
+  },
+  {
+    "revision": "3928094708cc04daa713",
+    "url": "/generations-client/static/js/2.333c60c0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/generations-client/static/js/2.333c60c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "054416f6e87ad85ff913",
+    "url": "/generations-client/static/js/main.6c7488bd.chunk.js"
+  },
+  {
+    "revision": "ea0b39098104796687d9",
+    "url": "/generations-client/static/js/runtime-main.7dabf9cd.js"
+  },
+  {
+    "revision": "c66e1bc585fd85bcd4f8ffcde6dba85a",
+    "url": "/generations-client/static/media/cloud.c66e1bc5.png"
+  },
+  {
+    "revision": "19a950d8c6fb7a42608e9240fe07f54b",
+    "url": "/generations-client/static/media/smallboy.19a950d8.jpg"
+  }
+]);
