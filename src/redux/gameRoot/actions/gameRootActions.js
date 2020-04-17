7eb7ee004b730d6f1ca8e7f6e3b2d1331c29a6ed
ldail <leondailofficial@gmail.com>
2020-04-16 22:15:43 -0700

    getting character data from redux stor enow

--- src/redux/gameRoot/actions/gameRootActions.js
+++ src/redux/gameRoot/actions/gameRootActions.js
@@ -1,9 +1,19 @@
 import { SET_CURRENT_CHARACTER } from "../types/gameRootTypes";
+import { getAllFamilyData } from "../../helpers/helpers";
 
-export const setCurrentCharacter = payload => ({
-  type: SET_CURRENT_CHARACTER,
-  payload: [0]
-});
+export const setCurrentCharacter = characterId => {
+  const characters = getAllFamilyData().characters;
+  const characterInfo = characters.find(character => character.id === characterId);
+  const partnerId = characterInfo.partnerId;
+  const payload = [characterId];
+  if (partnerId >= 0) {
+    payload.push(partnerId)
+  }
+  return ({
+    type: SET_CURRENT_CHARACTER,
+    payload
+  });
+};
 
 export const setCurrentCharacterAsync = characterId => {
   return (dispatch, getState) => {
