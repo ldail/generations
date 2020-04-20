d9927d2ceb6e1ecba4212f317cbd1cdb8162c712
ldail <leondailofficial@gmail.com>
2020-04-19 18:30:25 -0700

    removed logger from middlewares

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,8 +9,8 @@ middlewares.push(logger);
 middlewares.push(thunk);
 
 let enhancers = '';
-enhancers = compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(...middlewares));
-// enhancers = applyMiddleware(...middlewares)
+// enhancers = compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(...middlewares));
+enhancers = applyMiddleware(...middlewares)
 
 const store = createStore(rootReducer, enhancers);
 
