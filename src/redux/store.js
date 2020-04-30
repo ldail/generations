15521598537a1680974839ab6a10bf9106c6c9e7
ldail <leondailofficial@gmail.com>
2020-04-29 22:54:14 -0700

    chrome fix for height and width of map

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,8 +9,8 @@ const middlewares = [];
 middlewares.push(thunk);
 
 let enhancers = '';
-enhancers = compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(...middlewares));
-// enhancers = applyMiddleware(...middlewares)
+// enhancers = compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(...middlewares));
+enhancers = applyMiddleware(...middlewares)
 
 const store = createStore(rootReducer, enhancers);
 
